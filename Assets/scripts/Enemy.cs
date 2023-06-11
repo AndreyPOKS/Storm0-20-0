f6be1b2da57010cb2977e87620d9868d521be1c8
Andrew POKS44b <aaf19102003@gmail.com>
2023-06-11 17:56:07 +0300

    Patch 0.0.7
    UI противников

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json.Linq;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,19 +11,29 @@ public class Enemy : MonoBehaviour
     public int Hp;
     public string tagObject;
     private Animator anim;
+    public Image[] lives;
     public Transform item;
     private Material MathBlink;
     private Material MathDefault;
     private SpriteRenderer spriteRend;
+    public Sprite FullHp;
+    public Sprite EmptyHp;
 
 
 
     void Start()
     {
+        for (int i = 0; i < lives.Length; i++)
+        {
+
+            lives[i].enabled = false;
+        }
         Hp = MaxHp;
         spriteRend = GetComponent<SpriteRenderer>();
         MathBlink = Resources.Load("EnemyBlink", typeof(Material)) as Material;
         MathDefault = spriteRend.material;
+        Invoke("ResetMaterial", .2f);
+
 
     }
     void Update(){
@@ -30,7 +43,8 @@ public class Enemy : MonoBehaviour
     public void TakeDamage( int damage ){
         
         Hp -= damage;
-        spriteRend.material = MathBlink;
+        
+        
 
         if (Hp <= 0){
             Die();
@@ -38,6 +52,8 @@ public class Enemy : MonoBehaviour
         else{
             Invoke("ResetMaterial", .2f);
         }
+        this.UnSetHealth(Hp);
+        spriteRend.material = MathBlink;
     }
     void Die(){
         Destroy(gameObject);
@@ -50,6 +66,37 @@ public class Enemy : MonoBehaviour
         {
             Instantiate(item, transform.position, transform.rotation);
             Debug.Log("Chest Die");
+        }
+        
+    }
+    public void UnSetHealth(int Hp)
+    {
+        for (int i = 0; i < lives.Length; i++)
+        {
+            lives[i].enabled = false;
+        }
+        switch (Hp)
+        {
+            case 60:
+            case > 60:
+                lives[0].sprite = FullHp;
+                lives[0].enabled = true;
+                break;
+            case 40:
+            case > 40:
+
+                lives[1].enabled = true;
+                break;
+            case 20:
+            case > 20:
+                lives[2].enabled = true;
+                break;
+            case 0:
+            case > 0:
+                lives[3].enabled = true;
+                Die();
+                break;
+            
         }
     }
     void ResetMaterial()
