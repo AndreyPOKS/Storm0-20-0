5c374c67ad4650d903578a1961dc36a31d7fb389
Andrew POKS44b <aaf19102003@gmail.com>
2023-06-11 15:19:50 +0300

    patch 0.0.5
    Облака полностью готовы

--- Assets/scripts/Oblako.cs
+++ Assets/scripts/Oblako.cs
@@ -8,15 +8,21 @@ public class Oblako : MonoBehaviour
     public Vector2 direction;
     [Header("Скорость облаков")]
     public float speed;
-    private float ForUnitySpeed;
+    public float ForUnitySpeed;
+    private float Space;
+    private float HowNeedSecond;
 
     void Start()
     {
+        Space = 300;
         ForUnitySpeed = Random.Range(1.0f, speed);
+        HowNeedSecond = Space / ForUnitySpeed;
     }
     void Update()
     {
-
+        if (HowNeedSecond <= 0){Destroy(gameObject);}
+        else{HowNeedSecond = HowNeedSecond - Time.deltaTime;}
         transform.Translate(direction * ForUnitySpeed * Time.deltaTime);
     }
+
 }
