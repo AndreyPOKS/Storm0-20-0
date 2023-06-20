081bb3ab972aa5235ab8832a4a10e567d825865b
Andrew POKS44b <aaf19102003@gmail.com>
2023-06-20 18:33:12 +0300

    PAtch

--- Assets/scripts/CollisionHeal.cs
+++ Assets/scripts/CollisionHeal.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CollisionHeal : MonoBehaviour
 {
-    public int collisionHeal = 10;
+    public int collisionHeal;
     public string collisionTag;
+
+
+
 
     private void OnCollisionEnter2D(Collision2D coll)
     {
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -8,6 +8,7 @@ public class player : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     private Vector2 move;
+    private bool choice;
     public FixedJoystick joystick;
     [Header("Player Attack Settings")]
     public Transform attack_Point;
@@ -17,7 +18,6 @@ public class player : MonoBehaviour
     public float attack_range = 0.5f;
     public LayerMask enemyLayers;
     public LayerMask ChestLayers;
-
     [Header("Player Animation Settings")]
     public Animator animator;
 
@@ -25,38 +25,51 @@ public class player : MonoBehaviour
 
     public void Update()
     {
-        // move.x = Input.GetAxisRaw("Horizontal");
-        // move.y = Input.GetAxisRaw("Vertical");
-        move.x = joystick.Horizontal;
-        move.y = joystick.Vertical;
-        animator.SetFloat("HorizintalMove",move.x);
-        animator.SetFloat("VerticalMove",move.y);
+        if (Input.GetKeyDown(KeyCode.K))
+            choice = true;
+        
+        else if (Input.GetKeyDown(KeyCode.J))
+            choice = false;
+
+        if (choice)
+        {
+            move.x = Input.GetAxisRaw("Horizontal");
+            move.y = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            move.x = joystick.Horizontal;
+            move.y = joystick.Vertical;
+        }
+        animator.SetFloat("HorizintalMove", move.x);
+        animator.SetFloat("VerticalMove", move.y);
         rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
 
-        //if (Input.GetKeyDown(KeyCode.E))
-        //{
-        //    Attack(1);
-        //}
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            interaction();
+        }
 
 
-        // if (AttackCollDawnForUnity <= 0){
+        if (AttackCollDawnForUnity <= 0)
+        {
 
-        // if (Input.GetKeyDown(KeyCode.Space))
-        // {
-        //
-        // AttackCollDawnForUnity = AttackCollDawn;
-        //       animator.SetTrigger("Attack");
-        //
-        // 
-        //else{
-        //AttackCollDawnForUnity -= Time.deltaTime;
-        //}
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                AttackCollDawnForUnity = AttackCollDawn;
+                animator.SetTrigger("Attack");
+
+            }
+        }
+        else
+        {
+            AttackCollDawnForUnity -= Time.deltaTime;
+        }
     }
     public void Attack()
     {
-
         AttackCollDawnForUnity = AttackCollDawn;
-        animator.SetTrigger("Attack");
         AttackCollDawnForUnity -= Time.deltaTime;
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attack_Point.position, attack_range, enemyLayers);
             foreach (Collider2D enemy in hitEnemies)
@@ -65,8 +78,18 @@ public class player : MonoBehaviour
                 enemy.GetComponent<Enemy>().TakeDamage(damage);
             }
 
+    }   
+    public void interaction()
+    {
+        Debug.Log("ÄÀ");
+        Collider2D[] Objects = Physics2D.OverlapCircleAll(attack_Point.position, attack_range, ChestLayers);
+            foreach (Collider2D Heal in Objects)
+            {
+                Debug.Log(Heal);
+            }
+
     }
-   
+
     void OnDrawGizmosSelected(){
         if (attack_Point == null){
             return;
